71072b37e0688f3f7a348d0f43225182d88c26d3
Aiden <im.EndangeredFish@gmail.com>
2021-08-26 12:26:53 +0800

    bugfix: cannot update redis session repository settings with application.yml
    using @EnableRedisHttpSession which will result in Spring Boot's auto-configuration backing off

--- src/main/java/eu/openanalytics/containerproxy/util/RedisSessionHelper.java
+++ src/main/java/eu/openanalytics/containerproxy/util/RedisSessionHelper.java
@@ -25,14 +25,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.session.FindByIndexNameSessionRepository;
 import org.springframework.session.Session;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
 @Component
 @ConditionalOnProperty(prefix = "spring.session", name = "store-type", havingValue = "redis")
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 14400)
+//using @EnableRedisHttpSession which will result in Spring Boot's auto-configuration backing off.
 public class RedisSessionHelper {
 
     @Autowired
