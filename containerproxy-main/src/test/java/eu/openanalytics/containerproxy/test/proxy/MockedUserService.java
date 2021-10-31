9b5ab12676bfdaf70cc8b552f315ffdffa39e901
Steve Fan <29133953+stevefa19037626d@connect.polyu.hk>
2021-10-31 14:36:18 +0800

    fixup! merge containerproxy with shinyproxy

--- containerproxy-main/src/test/java/eu/openanalytics/containerproxy/test/proxy/MockedUserService.java
+++
@@ -1,9 +0,0 @@
-package eu.openanalytics.containerproxy.test.proxy;
-
-import eu.openanalytics.containerproxy.service.UserService;
-
-public class MockedUserService extends UserService {
-    public String getCurrentUserId() {
-        return "jack";
-    }
-}
